12c55d1b96594ef5c7dac5377a10e6adfc1217c5
[ThejMal01] <[thejani.mallawaarachchi@gmail.com]>
2023-10-27 00:56:02 +0530

    ongoing job

--- src/screens/User/Dashboard.js
+++ src/screens/User/Dashboard.js
@@ -61,15 +61,18 @@ const Dashboard = ({ navigation }) => {
             return (
               <Pressable
                 accessibilityLabel="More options menu"
-                {...triggerProps}>
+                {...triggerProps}
+              >
                 <HamburgerIcon />
               </Pressable>
             );
-          }}>
+          }}
+        >
           <Button
             onPress={() => {
               firebase.auth().signOut();
-            }}>
+            }}
+          >
             Sign Out
           </Button>
         </Menu>
@@ -80,13 +83,15 @@ const Dashboard = ({ navigation }) => {
           <View style={styles.cardContainer}>
             <TouchableOpacity
               onPress={() => navigation.navigate("Add New Expert")}
-              style={styles.card}>
+              style={styles.card}
+            >
               <Ionicons name="person-add" size={40} color="#3498db" />
               <Text style={styles.cardTitle}>Add New Expert</Text>
             </TouchableOpacity>
             <TouchableOpacity
               style={styles.card}
-              onPress={() => navigation.navigate("Expert Profile")}>
+              onPress={() => navigation.navigate("Expert Profile")}
+            >
               <Ionicons name="person-circle" size={40} color="#2ecc71" />
               <Text style={styles.cardTitle}>Expert Profile</Text>
             </TouchableOpacity>
@@ -96,15 +101,28 @@ const Dashboard = ({ navigation }) => {
           <View style={styles.cardContainer}>
             <TouchableOpacity
               style={styles.card}
-              onPress={() => navigation.navigate("Upcoming Jobs")}>
+              onPress={() => navigation.navigate("Upcoming Jobs")}
+            >
               <Ionicons name="calendar" size={40} color="#2ecc71" />
               <Text style={styles.cardTitle}>Upcoming Jobs</Text>
             </TouchableOpacity>
             <TouchableOpacity
               style={styles.card}
-              onPress={() => navigation.navigate("User Chat")}>
+              onPress={() => navigation.navigate("User Chat")}
+            >
               <Ionicons name="chatbubble-sharp" size={40} color="#2ecc71" />
               <Text style={styles.cardTitle}>User Chat</Text>
+            </TouchableOpacity>
+          </View>
+        </VStack>
+        <VStack>
+          <View style={styles.cardContainer}>
+            <TouchableOpacity
+              style={styles.card}
+              onPress={() => navigation.navigate("Ongoing Job")}
+            >
+              <Ionicons name="watch" size={40} color="#2ecc71" />
+              <Text style={styles.cardTitle}>Ongoing Job</Text>
             </TouchableOpacity>
           </View>
         </VStack>
