7786a0c5d9921322a86d2ffa88bc976c997f51b6
JaninduMunasinghe <munasin-ps20057@stu.kln.ac.lk>
2023-10-26 18:53:17 +0530

    add authentication

---
+++ config.js
@@ -0,0 +1,19 @@
+//firebase config key setup
+import firebase from "firebase/compat/app";
+import "firebase/compat/auth";
+import "firebase/compat/firestore";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCpLkRGAawz0Ux7Vr-wtK1e6Cm6R04L07A",
+  authDomain: "taskify-6ba79.firebaseapp.com",
+  projectId: "taskify-6ba79",
+  storageBucket: "taskify-6ba79.appspot.com",
+  messagingSenderId: "702526951889",
+  appId: "1:702526951889:web:5b97b1e9c3ee1f76d810f0",
+};
+
+if (!firebase.apps.length) {
+  firebase.initializeApp(firebaseConfig);
+}
+
+export { firebase };
---
+++ src/screens/Login/Registration.js
@@ -0,0 +1,162 @@
+import {
+  AspectRatio,
+  Box,
+  Image,
+  Text,
+  HStack,
+  Heading,
+  VStack,
+  Input,
+  Center,
+  Button,
+  FormControl,
+  Link,
+} from "native-base";
+import { View, TouchableOpacity, TextInput, StyleSheet } from "react-native";
+import React, { useState } from "react";
+import { firebase } from "../../../config";
+
+const Registration = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [firstName, setFirstName] = useState("");
+  const [lastName, setLastName] = useState("");
+
+  registerUser = async (email, password, firstName, lastName) => {
+    await firebase
+      .auth()
+      .createUserWithEmailAndPassword(email, password)
+      .then(() => {
+        firebase
+          .auth()
+          .currentUser.sendEmailVerification({
+            handleCodeInApp: true,
+            url: "https://taskify-6ba79.firebaseapp.com",
+          })
+          .then(() => {
+            alert("Email Verification Sent");
+          })
+          .catch((error) => {
+            alert(error.message);
+          })
+          .then(() => {
+            firebase
+              .firestore()
+              .collection("users")
+              .doc(firebase.auth().currentUser.uid)
+              .set({
+                firstName: firstName,
+                lastName: lastName,
+                email: email,
+                role: "user",
+              });
+          })
+          .catch((error) => {
+            alert(error.message);
+          });
+      })
+      .catch((error) => {
+        alert(error.message);
+      });
+  };
+
+  return (
+    <Center w="100%">
+      <Box safeArea p="2" w="90%" maxW="290" py="8">
+        <Heading
+          size="lg"
+          color="coolGray.800"
+          _dark={{
+            color: "warmGray.50",
+          }}
+          fontWeight="semibold">
+          Welcome
+        </Heading>
+        <Heading
+          mt="1"
+          color="coolGray.600"
+          _dark={{
+            color: "warmGray.200",
+          }}
+          fontWeight="medium"
+          size="xs">
+          Sign up to continue!
+        </Heading>
+        <VStack space={3} mt="5">
+          <FormControl>
+            <FormControl.Label>First Name</FormControl.Label>
+            <Input
+              placeholder="First Name"
+              onChangeText={(firstName) => setFirstName(firstName)}
+              autoCorrect={false}
+            />
+          </FormControl>
+          <FormControl>
+            <FormControl.Label>Last Name</FormControl.Label>
+            <Input
+              placeholder="Last Name"
+              onChangeText={(lastName) => setLastName(lastName)}
+              autoCorrect={false}
+            />
+          </FormControl>
+          <FormControl>
+            <FormControl.Label>Email</FormControl.Label>
+            <Input
+              placeholder="Email"
+              onChangeText={(email) => setEmail(email)}
+              autoCapitalize="none"
+              autoCorrect={false}
+              keyboardType="email-address"
+            />
+          </FormControl>
+          <FormControl>
+            <FormControl.Label>Password</FormControl.Label>
+            <Input
+              type="password"
+              placeholder="Password"
+              onChangeText={(password) => setPassword(password)}
+              autoCapitalize="none"
+              autoCorrect={false}
+              secureTextEntry={true}
+            />
+          </FormControl>
+          <Button
+            mt="2"
+            colorScheme="indigo"
+            onPress={() => registerUser(email, password, firstName, lastName)}>
+            Sign up
+          </Button>
+        </VStack>
+      </Box>
+    </Center>
+  );
+};
+
+export default Registration;
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: "center",
+    marginTop: 100,
+  },
+  Textinput: {
+    paddingTop: 20,
+    paddingBottom: 10,
+    width: 400,
+    fontSize: 20,
+    borderBottomWidth: 1,
+    borderBottomColor: "#000",
+    marginBottom: 10,
+    textAlign: "center",
+  },
+  button: {
+    marginTop: 50,
+    width: 250,
+    height: 70,
+    backgroundColor: "#026efd",
+    alignItems: "center",
+    justifyContent: "center",
+    borderRadius: 50,
+  },
+});
