ebf7004830cb89c0ada82343320b9482808ef3b7
Nisal <it21259302@my.sliit.lk>
2023-11-04 10:39:57 +0530

    ENV file added

--- config.js
+++ config.js
@@ -3,14 +3,22 @@ import firebase from "firebase/compat/app";
 import "firebase/compat/auth";
 import "firebase/compat/firestore";
 import "firebase/compat/storage";
+import {
+  EXPO_PRIVATE_FIREBASE_API_KEY,
+  EXPO_PRIVATE_FIREBASE_AUTH_DOMAIN,
+  EXPO_PRIVATE_FIREBASE_PROJECT_ID,
+  EXPO_PRIVATE_FIREBASE_STORAGE_BUCKET,
+  EXPO_PRIVATE_FIREBASE_MESSAGING_SENDER_ID,
+  EXPO_PRIVATE_FIREBASE_APP_ID,
+} from "@env";
 
 const firebaseConfig = {
-  apiKey: process.env.FIREBASE_API_KEY,
-  authDomain: process.env.FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.FIREBASE_PROJECT_ID,
-  storageBucket: process.env.FIRBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.FIREBASE_APP_ID,
+  apiKey: EXPO_PRIVATE_FIREBASE_API_KEY,
+  authDomain: EXPO_PRIVATE_FIREBASE_AUTH_DOMAIN,
+  projectId: EXPO_PRIVATE_FIREBASE_PROJECT_ID,
+  storageBucket: EXPO_PRIVATE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: EXPO_PRIVATE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: EXPO_PRIVATE_FIREBASE_APP_ID,
 };
 
 if (!firebase.apps.length) {
